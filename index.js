e654cadc9b7ef42d96f5b275e7dcc4b45a05888e
Samantha Grieco <samtb1@outlook.com>
2021-05-22 18:10:33 -0400

    added stressed response

--- index.js
+++ index.js
@@ -78,6 +78,45 @@ const suicidalMessageDM = {
     timestamp: new Date()
 };
 
+// Message that is first sent to the user 
+const stressedMessageDM = {
+    color: '#edf5f7',
+    title: 'Baymax Has A Message For You',
+    author: {
+        name: 'Mental Health Bot',
+        icon_url: 'http://clipart-library.com/data_images/134143.gif'
+    },
+    description: `A text you have sent in a server was flagged as containing Stressed Ideation. 
+    We know that life can get stressful at times, especially during school 
+    Baymax would love if you could take a step back, breathe, and remember that everything will be okay in the end`,
+    thumbnail: {
+        url: 'https://i.ibb.co/ZSWXnSW/Screen-Shot-2021-05-22-at-5-13-56-PM.png',
+    },
+    fields: [
+        {
+            name: '101 Reasons Why It Will All Be Okay',
+            value: 'https://www.anniewright.com/101-reasons-will-okay/',
+        },
+        {
+            name: 'A Guide to Burnout',
+            value: 'https://www.healthline.com/health/tips-for-identifying-and-preventing-burnout',
+        },
+        {
+            name: '10 Simple Ways to Relieve Stress',
+            value: 'https://www.healthline.com/health/10-ways-to-relieve-stress',
+        },
+        {
+            name: '14 Instant Ways to Calm Yourself Down',
+            value: 'https://www.everydayhealth.com/columns/therese-borchard-sanity-break/10-quick-ways-to-calm-down/',
+        },
+        {
+            name: 'Type the command below for a list of commands and helpful resources',
+            value: '!commands',
+        }
+    ],
+    timestamp: new Date()
+};
+
 // on ready function + all hard coded commands
 client.on('ready',() => {
     console.log('client is ready!')
@@ -131,7 +170,7 @@ let suicidalWordFound = false ;
     for (let i = 0 ; i < suicidalWords.length ; i++){
         console.log("checked " + suicidalWords[i])
         if (message.content.toLowerCase().includes(suicidalWords[i].toLowerCase())){
-            console.log("it has a sad word");
+            console.log("it has a sad word ####################################");
             // if a suicidal word is found
             
             suicidalWordFound = true ;
@@ -154,7 +193,7 @@ let suicidalWordFound = false ;
         for (let i = 0 ; i < stressfulWords.length ; i++){
             if (message.content.toLowerCase().includes(stressfulWords[i].toLowerCase())){
                 // if a stressed word is found
-    
+                message.author.send({ embed: stressedMessageDM });
                 break; // if one word is found then exit after, dont keep looping
             }
         }
