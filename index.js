563661999b37a7af8cfeb9e4a433c7df7027d98b
TheDe-bugger <jaimildalwadi2003@gmail.com>
2021-05-22 21:55:39 -0400

    reactions work

--- index.js
+++ index.js
@@ -354,8 +354,8 @@ client.on('messageReactionAdd', (reaction, user) => {
     if(user.id !== client.user.id) {
         if(reaction.message.id === msgId[0] && reaction.emoji.name == "👍") {
             user.send(`I'm glad I could help! \nPlease utilize the links above, along with other resources by typing !command`);
-            const the_channel = reaction.message.guild.channels.cache.find(varChannel => varChannel.name === 'baymax-bot-notifs');
-            the_channel.send('ayyy lets go');
+            //const the_channel = reaction.message.guild.channels.cache.find(varChannel => varChannel.name === 'baymax-bot-notifs');
+            //the_channel.send('ayyy lets go');
         } else if(reaction.message.id === msgId[0] && reaction.emoji.name == "👎") {
             user.send("Sorry for the inconvenience, but always remember you are wanted and loved ❤️");
             //user.send("feel free to use !command in the future if you want");
@@ -368,38 +368,5 @@ client.on('messageReactionAdd', (reaction, user) => {
     }
 });
 
-
-
-    // if it is in the list
-    // counter += 1
-    // counter > 10
-    // check time
-    // situationcheck()
-    // if else with api call to suicidal api - jaimil
-        // dm person and check if no/yes for error - jaimil
-            
-            //yes: apologize and send a nice message - jaimil
-
-            //timecheck: timestamp of the sent message, add an hour to it, if person doesnt respond send it
-            
-
-
-// listen function for all messages to check for stress/anxiety related to assessments - sam
-
-    // return some help and resources - jaimil
-
-    // a random choice from a list of destressing activities - sam 
-
-    // a random choice from a list of destressing gifs - anna
-    
-    // pinging mods in mod channel if it gets bad? - om
-
-
-// situationcheckkkkkkkkkkkk() hello
-// based on what time of year it is or the general situation  - anna
-
-    // automatically send gifs and um help/motivational comments every few minutes - anna
-
-    // ping mod - om
 
 client.login(config.token)
